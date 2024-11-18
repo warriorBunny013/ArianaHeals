5d865c94d2e099dd2772a046d4ec23c0bb97f125
warriorBunny013 <mona23sonai@gmail.com>
2024-11-19 02:57:32 +0530

    added translate issue

--- src/components/ui/container-scroll-animation.tsx
+++ src/components/ui/container-scroll-animation.tsx
@@ -54,7 +54,10 @@ export const ContainerScroll = ({
   );
 };
 
-export const Header = ({ translate, titleComponent }: any) => {
+export const Header = ({ translate, titleComponent }:  {
+  translate: MotionValue<number>;
+  titleComponent: string | React.ReactNode;
+}) => {
   return (
     <motion.div
       style={{
@@ -70,6 +73,7 @@ export const Header = ({ translate, titleComponent }: any) => {
 export const Card = ({
   rotate,
   scale,
+  translate,
   children,
 }: {
   rotate: MotionValue<number>;
@@ -82,6 +86,7 @@ export const Card = ({
       style={{
         rotateX: rotate,
         scale,
+        translateY: translate,
         boxShadow:
           "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
       }}
