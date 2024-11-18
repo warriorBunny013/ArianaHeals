9ead5042a31e54680c8cf1a822fe24f798399b58
warriorBunny013 <mona23sonai@gmail.com>
2024-11-19 02:46:07 +0530

    imported react

--- src/components/ui/about.tsx
+++ src/components/ui/about.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-
+import React from "react";
 const About: React.FC = () => {
  
 
--- src/components/ui/services.tsx
+++ src/components/ui/services.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
-
+import React from "react";
 export const Services: React.FC = () => {
     
     return (
