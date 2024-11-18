62824659faca3ba5a5f9198d831ec87fe841af40
warriorBunny013 <mona23sonai@gmail.com>
2024-11-19 01:22:06 +0530

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,295 @@
 import Image from "next/image";
+import { AnimatedTestimonials } from "@/components/ui/animated-testimonials";
+import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
+import { ContainerScroll } from "@/components/ui/container-scroll-animation";
+import About from "@/components/ui/about";
+import {Services} from "@/components/ui/services";
 
+// import dynamic from "next/dynamic";
+// import CardDemo from "@/components/blocks/cards-demo-2";
+
+// const NoSSRTestimonials = dynamic(() => AnimatedTestimonials, { ssr: false });
+const testimonialsData = [
+  {
+    quote: "Ariana's coaching provided a safe, empowering space for me to work through limiting beliefs. I feel more confident and connected to my path than ever before",
+    name: "Emily R",
+    designation: "Marketing Manager",
+    src: "/images/testimonial1.jpg",
+  },
+  {
+    quote: "Working with Ariana has been life-changing! Her insights into Human Design gave me tools I use daily to make better decisions and stay aligned with my purpose. I can't recommend her enough!",
+    name: "Sarah T.",
+    designation: "Wellness Entrepreneur",
+    src: "/images/testimonial2.jpg",
+  },
+  {
+    quote: "Ariana's coaching helped me reframe old thought patterns and focus on actionable steps to grow my business. I appreciate her thoughtful guidance and unique ability to bring out the best in others.",
+    name: "Michael S.",
+    designation: "Freelance Designer",
+    src: "/images/testimonial3.jpg",
+  },
+];
+
+const item = [
+  {
+    quote:
+      "Ariana’s natural ability to connect with others, combined with her authenticity, allows her to deeply understand her clients' needs and provide genuine support.",
+
+    title: "Empathy and Authenticity",
+  },
+  {
+    quote:
+      "Growing up with diverse cultural influences, Ariana is perceptive to different perspectives, making her adept at helping clients from all walks of life.",
+
+    title: "Cultural Awareness",
+  },
+  {
+    quote: "Ariana is passionate about growth and self-love. She helps clients evolve by guiding them through their journey of self-discovery and empowering them to recognize their own strengths.",
+
+    title: "Personal Growth Focus",
+  },
+  {
+    quote:
+      "With over a decade of experience in the hospitality industry, Ariana excels at making others feel seen, heard, and valued, creating a safe and welcoming space for her clients.",
+   
+    title: "Experience in Service and Hospitality",
+  },
+  {
+    quote:
+      "Ariana blends her knowledge of yoga, Reiki, and mindfulness practices to facilitate a well-rounded, nurturing experience for her clients.",
+   
+    title: "Holistic Approach",
+  },
+  {
+    quote:
+      "She is detail-oriented, considerate, and values honesty, growth, and integrity, ensuring that her clients receive tailored advice that aligns with their personal goals.",
+  
+    title: "Attention to Detail and Thoughtfulness",
+  },
+  {
+    quote:
+      "Ariana’s journey from hospitality to life coaching shows her dedication to empowering others, teaching clients how to embrace change, take risks, and live authentically.",
+ 
+    title: "Supportive Mentor",
+  },
+];
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      {/* HOME PAGE */}
+      <div className="overflow-x-hidden font-serif">
+      
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div 
+        
+          className="flex justify-center lg:justify-between "
+          style={{
+            background:
+              "linear-gradient(180deg, var(--black), var(--slate-900) ",
+          }}
+        >
+          <div className="lg:ml-20 flex justify-center">
+              <img src='/images/logoAriana.avif' className="w-[6rem] py-2" alt='logo'/>
+            </div>
+          
+          <div className="navbar-center hidden font-serif text-white mt-8 lg:flex">
+            <ul className="menu menu-horizontal flex mr-10 text-md font-extralight px-1">
+              <li><a href="#about" 
+              // onClick={() => navigateTo('about')} 
+              className="block py-2 px-4 hover:rounded-sm hover:duration-100 hover:bg-slate-900">About me</a></li>
+              <li><a href="#services" 
+              // onClick={() => navigateTo('services')} 
+              className="block py-2 px-4 hover:rounded-sm hover:duration-100 hover:bg-slate-900">Services</a></li>
+                <li><a href="#lifecoaching" 
+              // onClick={() => navigateTo('services')} 
+              className="block py-2 px-4 hover:rounded-sm hover:duration-100 hover:bg-slate-900">Life Coaching</a></li>
+              <li><a href="#testimonials" 
+              // onClick={() => navigateTo('testimonials')} 
+              className="block py-2 px-4 hover:rounded-sm hover:duration-100 hover:bg-slate-900">Testimonials</a></li>
+              <li><a href="#contact" 
+              // onClick={() => navigateTo('testimonials')} 
+              className="block py-2 px-4 hover:rounded-sm hover:duration-100 hover:bg-slate-900">Contact</a></li>
+            </ul>
+          </div>
+          {/* <div className="navbar-start">
+            <button onClick={toggleMenu} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 text-white"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
+           
+          </div> */}
+   
+ 
+     {/* <div className="navbar-end hidden lg:flex mr-10">
+            <a href="mailto:uditi013@gmail.com" onClick={handleContactClick} className="btn border-0 text-white font-serif font-extralight">Contact me</a>
+          </div> */}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        {/* Fullscreen Slider Menu */}
+        {/* <div className={`fixed top-0 right-0 h-full w-full transform ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out z-50`}>
+          <div className="flex justify-end p-4">
+            <button onClick={toggleMenu} className="text-black text-3xl">
+              &times;
+            </button>
+          </div>
+          <ul className="flex flex-col items-center justify-center h-full font-serif text-black text-2xl">
+            <li className="my-4"><a href="#about" onClick={() => navigateTo('about')} className="block py-2">About me</a></li>
+            <li className="my-4"><a href="#services" onClick={() => navigateTo('services')} className="block py-2">Services</a></li>
+            <li className="my-4"><a href="#testimonials" onClick={() => navigateTo('testimonials')} className="block py-2">Testimonials</a></li>
+          </ul>
+        </div> */}
+        {/* <div className="bg-[#dbe47c] flex  justify-center md:hidden">
+              <img src='logo.png' className='w-[12rem] pb-6' alt='logo'/>
+            </div> */}
+        {/* HERO SECTION STARTS FROM HERE!! */}
+        <div className="container mx-auto flex mt-10 flex-wrap md:flex-nowrap pl-10 pr-10  justify-center ">
+          <div className='pt-1 lg:pt-20 lg:pr-[10rem] lg:flex-col justify-center'>
+            
+            <div 
+              className='font-serif text-black mr-10 font-medium text-4xl leading-10 xl:text-5xl mb-4 '
+            >
+              Ready to <span className=' text-green-600 rounded-top-xl'>Unlock</span><br/> Your True Potential?
+            </div>
+            <div 
+              className=' lg:w-[65vh] pr-10 md:pr-0 text-sm lg:text-lg font-serif text-gray-600'
+            >
+            "Spiritual alignment through prana healing & coaching, human design enthusiast, and into all things mystical"
+            </div>
+            <div className="flex justify-end mt-2 mb-8">
+            <div 
+              className='rubik-wet-paint-regular text-sm lg:text-md italic mb-2 '
+            >
+              ~ Ariana Sadre
+            </div>
+            </div>
+            
+            <a 
+            href="#services" 
+            // onClick={() => navigateTo('newsletter')} 
+            
+            className=" shadow-black hover: scale-150 shadow-2xl bg-black rounded-md font-normal text-sm lg:text-lg px-10 text-white font-serif mb-10 border py-4 cursor-pointer ease-in hover:bg-gray-900 "
+            style={{
+              background:
+                "linear-gradient(180deg, var(--black), var(--slate-900) ",
+            }}
+            > 
+          Explore Services
+
+        
+          </a>
+          
+          </div>
+          <div className='w-[77vh]'>
+            <img src='/images/Ariana.avif' className="rounded-full mt-10 lg:mt-0" alt='hero'/>
+              
+          </div>
+        </div>
+      </div>
+      <div className="rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+        <InfiniteMovingCards items={item} direction="right"
+        speed="slow"/>
+      </div>
+
+      {/* ABOUT ME */}
+      <div id="about">
+      <About/>
+      </div>
+      
+ 
+      <div id="lifecoaching" className="flex flex-col overflow-hidden">
+      <ContainerScroll
+        titleComponent={
+          <>
+            <h1 className="text-3xl font-serif font-medium text-gray-700 dark:text-white">
+            Life coaching vs Therapy <br />
+              <span className="text-4xl md:text-[6rem] text-black font-bold mt-1 leading-none">
+              Holistic Approach Life Coaching 
+              </span>
+            </h1>
+          </>
+        }
+      >
+        <Image
+          src={`/images/approach2.png`}
+          alt="hero"
+          height={720}
+          width={1400}
+          className="mx-auto rounded-2xl object-cover h-full object-left-top"
+          draggable={false}
+        />
+      </ContainerScroll>
+    </div>
+
+    {/* SERVICES */}
+    <div id="services" className="">
+      <Services/>
+
+    </div>
+      {/* TESTIMONIALS */}
+      <div id="testimonials" className=" pt-20 border-b border-gray-600 text-white"
+       style={{
+        background:
+          "linear-gradient(120deg, var(--black), var(--slate-900) ",
+      }}
+      >
+      <h1 className="text-4xl font-serif font-medium text-center my-8">
+        What People Are Saying
+      </h1>
+      <AnimatedTestimonials testimonials={testimonialsData} autoplay={true} />
+      </div>
+
+      {/* FOOTER */}
+
+      <footer 
+      className="pt-10"
+       style={{
+        background:
+          "linear-gradient(180deg, var(--black), var(--slate-900) ",
+      }}>
+          <div className='w-full  mx-auto max-w-screen-xl p-4 md:flex gap-3 md:items-center md:justify-between'>
+            <div className='max-w-1/2 mb-10 text-sm text-gray-300'>
+              <span className='font-bold'>Disclaimer:</span> The life coaching services provided by Ariana Sadre, Holistic Life Coach, are designed to guide and support personal growth, alignment, and well-being. These services are not a substitute for medical, psychological, or professional advice. Results may vary, and all actions taken based on coaching sessions are the client's responsibility. Always consult appropriate professionals for health, legal, or financial concerns.
+            </div>
+          </div>
+          <div className="w-full mx-auto max-w-screen-xl p-4 md:flex gap-3 md:items-center md:justify-between">
+          <span className="text-sm text-gray-400">© 2024, Ariana Sadre | <span className="hover:underline"></span> All Rights Reserved.</span>
+              <ul className="md:flex hidden text-sm md:ml-auto items-center text-white">
+                <li><a href="#about" 
+                // onClick={() => navigateTo('about')} 
+                className="hover:underline me-4 cursor-pointer md:me-6 block py-2 px-4 text-white">About</a></li>
+                <li><a href="mailto:uditi013@gmail.com" 
+                // onClick={handleContactClick} 
+                className="hover:underline me-4 cursor-pointer md:me-6 block py-2 px-4 text-white">Contact</a></li>
+                <li><a href="#services" 
+                // onClick={() => navigateTo('services')} 
+                className="hover:underline me-4 cursor-pointer md:me-6 block py-2 px-4 text-white">Services</a></li>
+                <li><a href="#testimonials" 
+                // onClick={() => navigateTo('testimonials')} 
+                className="hover:underline me-4 cursor-pointer md:me-6 block py-2 px-4 text-white">Testimonials</a></li>
+              </ul>
+            
+          </div>
+          <div className="text-sm text-gray-300 pb-10 flex justify-center">
+  &#9734; Designed and Developed by{' '}
+  <a href="https://linktr.ee/UditiDas" target="_blank" rel="noopener noreferrer"  className="ml-1 underline">
+    Uditi Das
+  </a>
+</div>
+
+        </footer>
+     
     </div>
   );
 }
---
+++ src/components/ui/services.tsx
@@ -0,0 +1,103 @@
+"use client";
+import { motion } from "framer-motion";
+
+export const Services: React.FC = () => {
+    
+    return (
+       <div className="flex-wrap font-serif mb-[10rem]">
+            <div className="m-5 lg:ml-20 flex mb-[8rem] justify-center text-4xl font-medium tracking-tight text-black">Offerings</div>
+            <div
+                className="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-8 "
+            >
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
+                    
+                        <motion.div
+                            
+                            className="rounded overflow-hidden shadow-lg bg-white pb-3 flex flex-col"
+                            initial={{ opacity: 0, y: -100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.5, ease: "easeInOut" }}
+                        >
+                            <div className="relative">
+                                <img className="w-full" src="images/service1.jpg" alt="services" />
+                                <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25">
+                                </div>
+                            </div>
+                            <div className="px-6 py-4 mb-auto">
+                                <div className="text-lg text-[1.3rem] font-bold text-gray-900 hover:text-indigo-600 cursor-pointer transition duration-500 ease-in-out inline-block mb-2">Spiritual Alignment Session</div>
+                                <p className="text-gray-700 [word-spacing:2px] text-md">1 hr 30 min</p>
+                            </div>
+                            <div className="px-6 py-3 flex flex-row items-center justify-between">
+                                <button
+                                    className="ml-1 py-2 px-4   text-white"
+                                    style={{
+                                        background:
+                                          "linear-gradient(120deg, var(--black), var(--slate-900) ",
+                                          
+                                      }}
+                                    
+                                >
+                                    Book Now
+                                </button>
+                                <span className="ml-1 text-black font-bold text-xl">$111</span>
+                            </div>
+                        </motion.div>
+
+                        <motion.div
+                            
+                            className="rounded overflow-hidden shadow-lg bg-white pb-3 flex flex-col"
+                            initial={{ opacity: 0, y: -100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 1, ease: "easeInOut" }}
+                                    >
+                                        <div className="relative">
+                                            <img className="w-full" src="images/service2.jpg" alt="services" />
+                                            <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25"></div>
+                                        </div>
+                                        <div className="px-6 py-4 mb-auto">
+                                            <div className="text-lg text-[1.3rem] font-bold text-gray-900 hover:text-indigo-600 cursor-pointer transition duration-500 ease-in-out inline-block mb-2">6 or 12 Sessions</div>
+                                            <p className="text-gray-700 [word-spacing:2px] text-md">1 hr</p>
+                                        </div>
+                                        <div className="px-6 py-3 flex flex-row items-center justify-between">
+                                            <button
+                                                className="ml-1 bg-black py-2 px-4 text-white"
+                                                
+                                            >
+                                                Book Now
+                                            </button>
+                                            <span className="ml-1 text-black font-bold text-xl">From $600</span>
+                                        </div>
+                                    </motion.div>
+                                    <motion.div
+                            className="rounded overflow-hidden shadow-lg bg-white pb-3 flex flex-col"
+                            initial={{ opacity: 0, y: -100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 1.5, ease: "easeInOut" }}
+                                    >
+                                        <div className="relative">
+                                            <img className="w-full" src="images/service3.jpg" alt="services" />
+                                            <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25"></div>
+                                        </div>
+                                        <div className="px-6 py-4 mb-auto">
+                                            <div className="text-lg text-[1.3rem] font-bold text-gray-900 hover:text-indigo-600 cursor-pointer transition duration-500 ease-in-out inline-block mb-2">Introduction to Human Design</div>
+                                            <p className="text-gray-700 [word-spacing:2px] text-md">1 hr 30 min</p>
+                                        </div>
+                                        <div className="px-6 py-3 flex flex-row items-center justify-between">
+                                            <button
+                                                className="ml-1 bg-black py-2 px-4 text-white "
+                                                
+                                            >
+                                                Book Now
+                                            </button>
+                                            <span className="ml-1 text-black font-bold text-xl">$144</span>
+                                        </div>
+                                    </motion.div>
+                    
+                </div>
+            </div>
+            </div>
+
+    );
+  };
+  
+  
